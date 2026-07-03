8f93e484b7328e9b3818c2c3a3b68b04928ee6d5
vidkid69 <dhirajsah22003@gmail.com>
2026-07-03 23:13:26 +0545

    Prevent legacy about CMS content from showing

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -12,8 +12,14 @@ const defaultAbout = {
   photo: '/mukk-removebg-preview.png',
 }
 
+const legacyAboutTerms = ['community leader', 'youth champion', 'son of nepal', 'public service', 'kathmandu valley', 'people from all walks', 'prosperous nepal']
+function isLegacyAbout(content: typeof defaultAbout) {
+  return legacyAboutTerms.some(term => `${content.pill} ${content.heading} ${content.paragraphs}`.toLowerCase().includes(term))
+}
+
 export default function About() {
-  const content = useStoredData('about', defaultAbout)
+  const storedContent = useStoredData('about', defaultAbout)
+  const content = isLegacyAbout(storedContent) ? defaultAbout : storedContent
 
   return <AboutMukeshSection content={content} />
 }
