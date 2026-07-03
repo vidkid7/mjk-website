6e5253282683f760c9c89dbdb86300fe1885c127
vidkid69 <dhirajsah22003@gmail.com>
2026-07-03 23:06:01 +0545

    Update sitemap for portfolio and blog

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,15 @@
 import { MetadataRoute } from 'next'
 
-const routes = ['', '#services', '#products', '#projects', '#about', '#contact']
+const routes = ['', '#about', '#vision', '#initiatives', '#client-portfolio', '#insights', '#news', '#contact', 'blog']
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.aashatech.com'
+  const baseUrl = 'https://mukeshjungkhadka.com.np'
   const lastModified = new Date()
 
   return routes.map((route, index) => ({
-    url: route ? `${baseUrl}/${route}` : baseUrl,
+    url: route.startsWith('#') ? `${baseUrl}/${route}` : route ? `${baseUrl}/${route}` : baseUrl,
     lastModified,
-    changeFrequency: index === 0 ? 'weekly' : 'monthly',
-    priority: index === 0 ? 1 : 0.8,
+    changeFrequency: index === 0 || route === 'blog' ? 'weekly' : 'monthly',
+    priority: index === 0 ? 1 : route === 'blog' ? 0.85 : 0.8,
   }))
 }
